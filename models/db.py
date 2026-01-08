64c43d2b78ebecc81f935fa66bdef637c747e23b
Juan Andres <juanrevollo001@gmail.com>
2026-01-08 09:45:45 -0400

    Guardando cambios locales antes de sincronizar con remoto

--- models/db.py
+++ models/db.py
@@ -1,10 +1,9 @@
-# models/db.py
 import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",  # tu contraseña de XAMPP
-        database="probwebii"
+        host="sql305.infinityfree.com",        # Host de InfinityFree
+        user="if0_40092981",                   # Tu usuario MySQL
+        password="zxiFMO8HMDeD",               # Tu contraseña
+        database="if0_40092981_probwebii_db"  # Nombre de la base de datos
     )
