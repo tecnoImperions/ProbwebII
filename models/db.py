c2ec8c46745f71fc524787164fdb097281082821
tecnoImperions <juanrevollo001@gmail.com>
2026-01-15 08:34:00 -0400

    Delete models directory

--- models/db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_connection():
-    return mysql.connector.connect(
-        host="Juanc101.mysql.pythonanywhere-services.com",
-        user="Juanc101",
-        password="Juan#8913394",
-        database="Juanc101$default"
-    )
